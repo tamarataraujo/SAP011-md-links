71bf566ba67e3e09103b074e36f957416b595b22
tamarataraujo <tamarataraujo@gmail.com>
2023-12-04 12:42:46 -0300

    extração dos arquivos mdLins

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  // ...
-};
+const fs = require('fs');
+
+function mdLinks(caminhoDoArquivo){
+  return new Promise(function(resolve,reject){
+    fs.readFile(caminhoDoArquivo, 'utf8', (err,data) => {
+      if (err) reject(err);
+      const pattern = /\[([^\]]+)\]\((https?[^)]+)\)/g;
+      const matches = [...data.matchAll(pattern)];
+      const links = matches.map(match => {
+        return {
+          href: match[2],
+          text: match[1],
+          file: caminhoDoArquivo
+        }
+      })
+      resolve(links);
+    });
+    });
+  }
+
+  mdLinks('./README.md').then(result => console.log(result))
+
+
+  module.exports = (mdLinks)
+
